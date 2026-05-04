b31798cc42c4d0588e6397a53e0bdde2346d55ed
Kalpesh Jain <jainkalpesh597@gmail.com>
2026-05-04 22:30:09 +0530

    Day 13 - "Daily Temperatures (Monotonic Stack)"

---
+++ src/leetcode75/DailyTemperatures.java
@@ -0,0 +1,38 @@
+package leetcode75;
+
+import java.util.Stack;
+
+public class DailyTemperatures {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+        int[] result = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+
+            stack.push(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] temps = {73,74,75,71,69,72,76,73};
+
+        DailyTemperatures obj = new DailyTemperatures();
+        int[] result = obj.dailyTemperatures(temps);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
