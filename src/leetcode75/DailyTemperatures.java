63a25c31128d2959b27050319eabd6c44b32eb37
Kalpesh Jain <jainkalpesh597@gmail.com>
2026-05-12 22:47:59 +0530

    "revision"

--- src/leetcode75/DailyTemperatures.java
+++ src/leetcode75/DailyTemperatures.java
@@ -26,7 +26,7 @@ public class DailyTemperatures {
 
     public static void main(String[] args) {
 
-        int[] temps = {73,74,75,71,69,72,76,73,81,83};
+        int[] temps = {73,74,75,71,69,72,76,73};
 
         DailyTemperatures obj = new DailyTemperatures();
         int[] result = obj.dailyTemperatures(temps);
